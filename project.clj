c96ad9915980638ae2448d85b1af0aa453e05e3f
Clyde Tan <bokbokan@gmail.com>
2016-06-25 10:41:20 +0800

    Fix project description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject kotlinc-lein "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Leiningen plugin for compiling Kotlin sources"
+  :url "https://github.com/bokbokan/kotlinc-lein"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
