4ff971ce5451670ad23c0bbd63f5092401b37f05
Clyde Tan <bokbokan@gmail.com>
2016-06-25 09:22:10 +0800

    Initial Commit - 0.1.0

---
+++ README.md
@@ -0,0 +1,89 @@
+# kotlinc-lein
+
+A Leiningen plugin for compiling Kotlin source files. It behaves similarly with the Leiningen javac plugin.
+
+####Latest version:
+
+```clojure
+[kotlinc-lein "0.1.0"]
+```
+
+Include the dependency as shown above in your project.clj or `:user` of profiles.clj
+
+## Source Layout
+
+By default, Leiningen assumes your project only has Clojure source code under
+`src`. When using both Clojure and Kotlin in the same codebase, however, it is
+necessary to tell Leiningen where to find Kotlin sources.
+
+To do so, use `:source-paths` and `:kotlin-source-path` options in the project
+definition:
+
+```clojure
+(defproject megacorp/superservice "1.0.0-SNAPSHOT"
+  :description "A Clojure project with a little bit of Kotlin sprinkled here and there"
+  :min-lein-version    "2.0.0"
+  :source-paths        ["src/clojure"]
+  :kotlin-source-paths ["src/kotlin"])
+```
+
+Having one source root contain another (e.g. `src` and `src/kotlin`) can
+cause obscure problems.
+
+## Kotlin Source Compilation
+
+To compile Kotlin sources, you can run
+
+    $ lein kotlinc
+
+To automatically compile Kotlin sources add `"kotlinc"` to the `:prep-tasks` vector
+
+## Setting Kotlin Compiler Options With Leiningen
+
+When compiling Kotlin sources, it may be necessary to pass extra arguments to the
+compiler. For example, it is very important to target the version of the generated
+JVM bytecodes. This can be done via the `:kotlinc-options` which takes a vector of
+arguments as you would pass them to `kotlinc` on the command line. In this case we want to generate
+JVM bytecode `1.6` and to use Kotlin language version `1.0`:
+
+```clojure
+(defproject megacorp/superservice "1.0.0-SNAPSHOT"
+  :description "A Clojure project with a little bit of Kotlin sprinkled here and there"
+  :min-lein-version    "2.0.0"
+  :source-paths        ["src/clojure"]
+  :kotlin-source-paths ["src/kotlin"]
+  :kotlinc-options     ["-jvm-target" "1.6" "-language-version" "1.0"])
+```
+
+Equivalently through the command line:
+
+    $ lein kotlinc -jvm-target 1.6 -language-version 1.0
+
+Take note that the example options above are only available when using the snapshot of the
+Kotlin compiler version `1.1`
+
+## Default Kotlin Compiler Version (1.0.2)
+
+To use different version of the Kotlin compiler, you can do it like so:
+
+```clojure
+(defproject megacorp/superservice "1.0.0-SNAPSHOT"
+  :description "A Clojure project with a little bit of Kotlin sprinkled here and there"
+  :min-lein-version        "2.0.0"
+  :source-paths            ["src/clojure"]
+  :kotlin-source-paths     ["src/kotlin"]
+  :kotlinc-options         ["-jvm-target" "1.6" "-language-version" "1.0"])
+  :kotlin-compiler-version "1.1-SNAPSHOT"
+  :repositories            [["snapshots" "http://oss.sonatype.org/content/repositories/snapshots"]]
+```
+
+## Kotlin Runtime Dependencies
+
+`[org.jetbrains.kotlin/kotlin-runtime "1.0.2"]` should be added to `:dependencies`
+for successful compilation. Make sure to match for the compiler version used.
+
+## License
+
+Copyright © 2016 Clyde Tan
+
+Distributed under the Eclipse Public License either version - v 1.0.
---
+++ src/leiningen/kotlinc.clj
@@ -0,0 +1,91 @@
+(ns leiningen.kotlinc
+  "Compile Kotlin source files"
+  (:require [leiningen.classpath :as classpath]
+            [clojure.string :as string]
+            [leiningen.core.eval :as eval]
+            [leiningen.core.main :as main]
+            [clojure.java.io :as io]
+            [leiningen.core.project :as project])
+  (:import [java.io File]))
+
+(defn- kotlin-sources
+  "Returns a lazy seq of file paths: every kotlin source file within dirs."
+  [dirs]
+  (for [dir dirs
+        ^File source (filter #(-> ^File % (.getName) (.endsWith ".kt"))
+                       (file-seq (io/file dir)))]
+    (.getPath source)))
+
+(defn- safe-quote [s]
+  (str "\"" (string/replace s "\\" "\\\\") "\"")
+  s)
+
+;; Tool's .run method expects the last argument to be an array of
+;; strings, so that's what we'll return here.
+(defn- kotlinc-options
+  "Compile all sources of possible options and add important defaults.
+  Result is a String java array of options."
+  [project files args]
+  (into-array String
+    (filter #(not-empty %) (flatten ["-cp" (safe-quote (classpath/get-classpath-string project))
+                                     "-d" (safe-quote (:compile-path project)) "-nowarn"
+                                     (:kotlinc-options project) args (map safe-quote files)]))))
+
+;; Pure kotlin projects will not have Clojure on the classpath. As such, we need
+;; to add it if it's not already there.
+(def subprocess-profile
+  {:dependencies [^:displace ['org.clojure/clojure (clojure-version)]]
+   :eval-in :subprocess})
+
+(defn compiler-profile
+  [version]
+  {:dependencies [['org.jetbrains.kotlin/kotlin-compiler version]]})
+
+(defn- subprocess-form
+  "Creates a form for running kotlinc in a subprocess."
+  [compile-path files kotlinc-opts]
+  (main/debug "Running kotlinc with" kotlinc-opts)
+  `(do
+     (binding [*out* *err*]
+       (println "Compiling" ~(count files) "source files to" ~compile-path))
+     (.mkdirs (clojure.java.io/file ~compile-path))
+     (org.jetbrains.kotlin.cli.jvm.K2JVMCompiler/main (into-array String ~kotlinc-opts))))
+
+;; We can't really control what is printed here. We're just going to
+;; allow `.run` to attach in, out, and err to the standard streams. This
+;; should have the effect of compile errors being printed. kotlinc doesn't
+;; actually output any compilation info unless it has to (for an error)
+;; or you make it do so with `-verbose`.
+(defn- run-kotlinc-subprocess
+  "Run kotlinc to compile all source files in the project. The compilation is run
+  in a subprocess to avoid it from adding the leiningen standalone to the
+  classpath, as leiningen adds itself to the classpath through the
+  bootclasspath."
+  [project args]
+  (let [compile-path (:compile-path project)
+        files (kotlin-sources (:kotlin-source-paths project))
+        kotlinc-opts (vec (kotlinc-options project files args))
+        version (or (:kotlin-compiler-version project) "1.0.2")
+        form (subprocess-form compile-path files kotlinc-opts)]
+    (when (seq files)
+      (try
+        (binding [eval/*pump-in* false]
+          (eval/eval-in
+            (dissoc (project/merge-profiles (project/merge-profiles project [subprocess-profile])
+                      [(compiler-profile version)]) :prep-tasks)
+            form))
+        (catch Exception e
+          (if-let [exit-code (:exit-code (ex-data e))]
+            (main/exit exit-code)
+            (throw e)))))))
+
+(defn kotlinc
+  "Compile Kotlin source files
+
+Add a :kotlin-source-paths key to project.clj to specify where to find them.
+Options passed in on the command line as well as options from the :kotlinc-options
+vector in project.clj will be given to the compiler; e.g. `lein kotlinc -verbose`.
+Like the compile and deps tasks, this should be invoked automatically when
+needed and shouldn't ever need to be run by hand."
+  [project & args]
+  (run-kotlinc-subprocess project args))
